f00008dc70893933d10bad7109ebbb15798f8217
ArtemTsi <a.tsipotan.93@gmail.com>
2022-08-18 20:38:16 +0300

    server change

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,12 +2,14 @@ const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const sessionConfig = require('./session-config');
+// const test = require('../routes/api/test');
 
 const config = (app) => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(cookieParser());
   app.use(session(sessionConfig));
+  // app.use('/', test);
 };
 
 module.exports = config;
