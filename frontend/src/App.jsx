42c89d7017aa88421ae0787ba58dc3ac64d50742
Alexander Simanchev <alexander.simanchev@gmail.com>
2022-08-19 13:59:44 +0300

    input fetch logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import './App.css';
+import FinDataForm from './features/finData/FinDataForm';
 
 function App() {
   return (
     <div className="container">
-      <form>
-        <div className="mb-3 data-input-wrap">
-          <h6>Поступления</h6>
-          <div className="data-input-section">
-            <input type="input" className="form-control" placeholder="Название операции" />
-            <input type="date" className="form-control date-control" placeholder="Дата" />
-            <div className="form-check">
-              <input className="form-check-input flex-check" type="checkbox" value="" />
-              <label className="form-check-label">Ежемесячно</label>
-            </div>
-          </div>
-        </div>
-        <button type="submit" className="btn btn-primary">+</button>
-      </form>
+      <FinDataForm />
     </div>
   );
 }
