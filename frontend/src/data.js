1cbf1af5b36a369a9dc4c28f64b366a5049b190b
Damir <demchikbig777@mail.ru>
2022-08-19 17:20:37 +0300

    Created test file data.js

---
+++ frontend/src/data.js
@@ -0,0 +1,97 @@
+export const UserData = [
+  {
+    id:1,
+    month: 'January',
+    year: 2022,
+    income: Math.floor(Math.random() * 1000000) + 1,
+    cost: Math.floor(Math.random() * 1000000) - 1,
+  },
+
+  {
+    id:2,
+    month: 'February',
+    year: 2022,
+    income: Math.floor(Math.random() * 1000000) + 1,
+    cost: Math.floor(Math.random() * 1000000) - 1,
+  },
+
+  {
+    id:3,
+    month: 'March',
+    year: 2022,
+    income: Math.floor(Math.random() * 1000000) + 1,
+    cost: Math.floor(Math.random() * 1000000) - 1,
+  },
+
+  {
+    id:4,
+    month: 'April',
+    year: 2022,
+    income: Math.floor(Math.random() * 1000000) + 1,
+    cost: Math.floor(Math.random() * 1000000) - 1,
+  },
+
+  {
+    id:5,
+    month: 'May',
+    year: 2022,
+    income: Math.floor(Math.random() * 1000000) + 1,
+    cost: Math.floor(Math.random() * 1000000) - 1,
+  },
+
+  {
+    id:6,
+    month: 'June',
+    year: 2022,
+    income: Math.floor(Math.random() * 1000000) + 1,
+    cost: Math.floor(Math.random() * 1000000) - 1,
+  },
+
+  {
+    id:7,
+    month: 'July',
+    year: 2022,
+    income: Math.floor(Math.random() * 1000000) + 1,
+    cost: Math.floor(Math.random() * 1000000) - 1,
+  },
+
+  {
+    id:8,
+    month: 'August',
+    year: 2022,
+    income: Math.floor(Math.random() * 1000000) + 1,
+    cost: Math.floor(Math.random() * 1000000) - 1,
+  },
+
+  {
+    id:9,
+    month: 'September',
+    year: 2022,
+    income: Math.floor(Math.random() * 1000000) + 1,
+    cost: Math.floor(Math.random() * 1000000) - 1,
+  },
+
+  {
+    id:10,
+    month: 'October',
+    year: 2022,
+    income: Math.floor(Math.random() * 1000000) + 1,
+    cost: Math.floor(Math.random() * 1000000) - 1,
+  },
+
+  {
+    id:11,
+    month: 'November',
+    year: 2022,
+    income: Math.floor(Math.random() * 1000000) + 1,
+    cost: Math.floor(Math.random() * 1000000) - 1,
+  },
+
+  {
+    id:12,
+    month: 'December',
+    year: 2022,
+    income: Math.floor(Math.random() * 1000000) + 1,
+    cost: Math.floor(Math.random() * 1000000) - 1,
+  },
+]
