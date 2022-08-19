a27d1e0b30355dc4e33a103874bdc75b2772b2b7
Alexander Simanchev <alexander.simanchev@gmail.com>
2022-08-19 10:07:38 +0300

    initial form added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>CF Runway Project</p>
-      </header>
+    <div className="container">
+      <form>
+        <div className="mb-3 data-input-wrap">
+          <h6>Поступления</h6>
+          <div className="data-input-section">
+            <input type="input" className="form-control" placeholder="Название операции" />
+            <input type="date" className="form-control date-control" placeholder="Дата" />
+            <div className="form-check">
+              <input className="form-check-input flex-check" type="checkbox" value="" />
+              <label className="form-check-label">Ежемесячно</label>
+            </div>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">+</button>
+      </form>
     </div>
   );
 }
