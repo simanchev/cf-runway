753c33fe3c6f706d0295f0b1e89889e92f760d07
Alexander Simanchev <alexander.simanchev@gmail.com>
2022-08-20 10:51:53 +0300

    schedules seed updated

--- backend/db/seeders/20220818170710-schedules.js
+++ backend/db/seeders/20220818170710-schedules.js
@@ -1,25 +1,25 @@
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up(queryInterface) {
     await queryInterface.bulkInsert('Schedules', [{
       fin_data_id: 1,
-      1: 150000,
-      2: 150000,
-      3: 150000,
-      4: 150000,
-      5: 150000,
-      6: 150000,
-      7: 150000,
-      8: 150000,
-      9: 150000,
-      10: 150000,
-      11: 150000,
-      12: 150000,
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 50000,
+      5: 50000,
+      6: 50000,
+      7: 50000,
+      8: 50000,
+      9: 50000,
+      10: 50000,
+      11: 0,
+      12: 0,
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
   },
 
-  async down(queryInterface, Sequelize) {
+  async down(queryInterface) {
     await queryInterface.bulkDelete('Schedules', null, {});
   },
 };
