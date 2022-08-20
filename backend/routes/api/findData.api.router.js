05f07b4fac152c59404423dbc338cffc2da1e7de
Alexander Simanchev <alexander.simanchev@gmail.com>
2022-08-20 23:13:00 +0300

    db updated - schedule&findata merged

--- backend/routes/api/findData.api.router.js
+++ backend/routes/api/findData.api.router.js
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
 const finDataRouter = require('express').Router();
-const { Fin_data, Schedule } = require('../../db/models');
+const { Fin_data } = require('../../db/models');
 
 finDataRouter.get('/project/:id/findata', async (req, res) => {
   const { id } = req.params;
@@ -20,7 +20,35 @@ finDataRouter.post('/project/:id/findata/new', async (req, res) => {
   } = req.body;
 
   try {
-    const newFinData = await Fin_data.create({
+    const userStartYear = Number(start_date.slice(0, 4));
+    const userStartMonth = Number(start_date.slice(5, 7));
+
+    const userEndYear = Number(end_date.slice(0, 4));
+    const userEndMonth = Number(end_date.slice(5, 7));
+
+    const curYear = new Date().getFullYear();
+    const curMonth = new Date().getMonth() + 1;
+
+    let firstFillMonth = null;
+    let lastFillMonth = null;
+
+    if (curYear === userStartYear && userStartMonth >= curMonth) {
+      firstFillMonth = userStartMonth - curMonth + 1;
+    } else if (userStartYear - curYear === 1 && userStartMonth < curMonth) {
+      firstFillMonth = 12 - curMonth + 1 + userStartMonth;
+    }
+
+    if (userEndYear) {
+      if (curYear === userEndYear && userEndMonth >= curMonth) {
+        lastFillMonth = userEndMonth - curMonth + 1;
+      } else if (userEndYear - curYear === 1 && userEndMonth < curMonth) {
+        lastFillMonth = 12 - curMonth + 1 + userEndMonth;
+      }
+    } else {
+      lastFillMonth = 12;
+    }
+
+    const newFinData = {
       project_id: id,
       fin_types_id,
       title,
@@ -28,10 +56,6 @@ finDataRouter.post('/project/:id/findata/new', async (req, res) => {
       regular,
       start_date,
       end_date,
-    });
-
-    const scheduleData = {
-      fin_data_id: newFinData.id,
       1: 0,
       2: 0,
       3: 0,
@@ -46,45 +70,17 @@ finDataRouter.post('/project/:id/findata/new', async (req, res) => {
       12: 0,
     };
 
-    const userStartMonth = Number(start_date.slice(0, 4));
-    const userStartYear = Number(start_date.slice(5, 7));
-
-    const userEndMonth = Number(start_date.slice(0, 4));
-    const userEndYear = Number(start_date.slice(5, 7));
-
-    const curYear = new Date().getFullYear();
-    const curMonth = new Date().getMonth() + 1;
-
-    let firstFillMnth = null;
-    let lastFillMnth = null;
-
-    if (curYear === userStartYear && userStartMonth >= curMonth) {
-      firstFillMnth = userStartMonth - curMonth + 1;
-    } else if (userStartYear - curYear === 1 && userStartMonth < curMonth) {
-      firstFillMnth = 12 - curMonth + 1 + userStartMonth;
-    }
-
-    if (curYear === userEndYear && userEndMonth >= curMonth) {
-      lastFillMnth = userEndMonth - curMonth + 1;
-    } else if (userEndYear - curYear === 1 && userEndMonth < curMonth) {
-      lastFillMnth = 12 - curMonth + 1 + userEndMonth;
-    }
-
-    console.log('firstFillMnth:', firstFillMnth);
-    console.log('lastFillMnth:', lastFillMnth);
-
     for (let i = 1; i <= 12; i++) {
-      if (i === firstFillMnth) {
-        scheduleData[i] = sum;
-      } else if (
-        i > firstFillMnth
-        && i <= lastFillMnth
-        && userStartYear - curYear <= 1
-        && regular === true) {
-        scheduleData[i] = sum;
+      if (i === firstFillMonth) {
+        newFinData[i] = sum;
+      // eslint-disable-next-line max-len
+      // } else if (i > firstFillMonth && userStartYear - curYear <= 1 && regular === true) {
+      } else if (i > firstFillMonth && i <= lastFillMonth && userStartYear - curYear <= 1 && regular === 'true') {
+        newFinData[i] = sum;
       }
     }
-    await Schedule.create(scheduleData);
+
+    await Fin_data.create(newFinData);
     res.status(201).json({ created: true });
   } catch (err) {
     res.status(500).json({ created: false, error: 'Не удалось сохранить операцию' });
@@ -119,7 +115,6 @@ finDataRouter.delete('/findata/:id', async (req, res) => {
 
   try {
     await Fin_data.destroy({ where: { id } });
-    console.log('!!!!!!!!!!!!', id);
     res.status(200).json({ deleted: true });
   } catch (err) {
     res.status(500).json({ deleted: false, error: 'Не удалось удалить операцию' });
