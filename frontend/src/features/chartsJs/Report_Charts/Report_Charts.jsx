e70de2e7ec89055dea67aa3938e5d62a674a0ed4
Damir <demchikbig777@mail.ru>
2022-08-22 12:15:32 +0300

    Report_Charts fixed

--- frontend/src/features/chartsJs/Report_Charts/Report_Charts.jsx
+++ frontend/src/features/chartsJs/Report_Charts/Report_Charts.jsx
@@ -1,6 +1,7 @@
 import Bar_Line_Chart from "../Bar_Line_Chart/Bar_Line_Chart.jsx";
 import Doughnut_Chart_Costs from "../Doughnut_Chart/Doughnut_Chart_Costs.jsx";
 import { Chart as ChartJS } from "chart.js/auto"
+import Doughnut_Chart_Income from "../Doughnut_Chart/Doughnut_Chart_Income.jsx";
 
 
 
@@ -14,7 +15,7 @@ function Report_Charts() {
     <div id="charts_style" >
       <Bar_Line_Chart />
       <br />
-      <br />
+      <Doughnut_Chart_Income />
       <br />
       <Doughnut_Chart_Costs />
       <br />
