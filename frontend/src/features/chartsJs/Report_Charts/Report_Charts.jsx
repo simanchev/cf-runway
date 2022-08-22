81e573becac87fbd80277742070b42cc827be1bd
Damir <demchikbig777@mail.ru>
2022-08-22 12:30:07 +0300

    Report_Charts 4.0

--- frontend/src/features/chartsJs/Report_Charts/Report_Charts.jsx
+++ frontend/src/features/chartsJs/Report_Charts/Report_Charts.jsx
@@ -1,7 +1,9 @@
 import Bar_Line_Chart from "../Bar_Line_Chart/Bar_Line_Chart.jsx";
 import Doughnut_Chart_Costs from "../Doughnut_Chart/Doughnut_Chart_Costs.jsx";
-import { Chart as ChartJS } from "chart.js/auto"
 import Doughnut_Chart_Income from "../Doughnut_Chart/Doughnut_Chart_Income.jsx";
+import '../Report_Charts/Report_Charts_Style.css'
+import { Chart as ChartJS } from "chart.js/auto"
+
 
 
 
