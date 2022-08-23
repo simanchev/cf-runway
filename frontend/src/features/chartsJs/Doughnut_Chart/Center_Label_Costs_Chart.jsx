469cf65206b3f852c4b55a9b6ae6f94300e72015
Damir <demchikbig777@mail.ru>
2022-08-23 16:14:08 +0300

    Center_Label_Costs 2.0

--- frontend/src/features/chartsJs/Doughnut_Chart/Center_Label_Costs_Chart.jsx
+++ frontend/src/features/chartsJs/Doughnut_Chart/Center_Label_Costs_Chart.jsx
@@ -20,7 +20,7 @@ function Center_Label_Costs_Chart({ costChartData }) {
       <div id='Center_Label'>
         Оплаты:
         <br />
-        {`${sumCosts} ₽`}
+        {`${sumCosts.toLocaleString()} ₽`}
       </div>
     </>
   );
