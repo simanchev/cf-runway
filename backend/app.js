aa8b784ff8e7617e37e7a374ec3eddb55850098d
Damir <demchikbig777@mail.ru>
2022-08-23 18:25:40 +0300

    app fixed

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const config = require('./config/config');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT ?? 4000;
+const PORT = process.env.PORT;
 
 config(app);
 
