1ece4d2804a1bb9ab37c1822c66022c464dcf7a0
Alexander Simanchev <alexander.simanchev@gmail.com>
2022-08-25 23:43:20 +0300

    seeds updated

--- backend/db/seeders/20220818170533-users.js
+++ backend/db/seeders/20220818170533-users.js
@@ -2,7 +2,7 @@ module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert('Users', [{
       name: 'Александр',
-      email: 'lexa@lexa',
+      email: 'serega@spasibo.tebe',
       password: '$2b$10$1d5MzSV2YsX1mkdsJFJ6yu7Ucf8CmZj/uxqQS4VVrWm2TTNPPiG0y',
       createdAt: new Date(),
       updatedAt: new Date(),
--- frontend/src/features/main/MainContent.jsx
+++ frontend/src/features/main/MainContent.jsx
@@ -9,10 +9,10 @@ function MainContent() {
       <div className="container main-container">
         <div className="welcome-wrap">
           <h3 className="main-title">Планируйте денежные потоки вашего бизнеса</h3>
-          <h6>Прогнозирование денежных потоков и оценка риска кассовых разрывов онлайн</h6>
+          <h6>Онлайн прогнозирование денежных потоков и оценка риска кассовых разрывов</h6>
           <h6>Быстро. Удобно. Бесплатно</h6>
           {localStorage.user
-            ? <button type="button" className="btn btn-warning btn-welcome" onClick={() => navigate('/profile')}>Рассчитать</button>
+            ? <button type="button" className="btn btn-warning btn-welcome" onClick={() => navigate('/profile')}>Начать сейчас</button>
             :
             <button type="button" className="btn btn-warning btn-welcome" data-bs-toggle="modal" data-bs-target="#regModal">Рассчитать</button>}
         </div>
