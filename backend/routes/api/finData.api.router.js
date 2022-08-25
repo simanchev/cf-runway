467111d53f53c9d9cfc5c4b784713176c62c185a
Alexander Simanchev <alexander.simanchev@gmail.com>
2022-08-25 18:28:08 +0300

    db updated

--- backend/routes/api/finData.api.router.js
+++ backend/routes/api/finData.api.router.js
@@ -27,34 +27,6 @@ finDataRouter.post('/project/:id/findata/new', async (req, res) => {
   } = req.body;
 
   try {
-    const userStartYear = Number(start_date.slice(0, 4));
-    const userStartMonth = Number(start_date.slice(5, 7));
-
-    const userEndYear = Number(end_date.slice(0, 4));
-    const userEndMonth = Number(end_date.slice(5, 7));
-
-    const curYear = new Date().getFullYear();
-    const curMonth = new Date().getMonth() + 1;
-
-    let firstFillMonth = null;
-    let lastFillMonth = null;
-
-    if (curYear === userStartYear && userStartMonth >= curMonth) {
-      firstFillMonth = userStartMonth - curMonth + 1;
-    } else if (userStartYear - curYear === 1 && userStartMonth < curMonth) {
-      firstFillMonth = 12 - curMonth + 1 + userStartMonth;
-    }
-
-    if (userEndYear) {
-      if (curYear === userEndYear && userEndMonth >= curMonth) {
-        lastFillMonth = userEndMonth - curMonth + 1;
-      } else if (userEndYear - curYear === 1 && userEndMonth < curMonth) {
-        lastFillMonth = 12 - curMonth + 1 + userEndMonth;
-      } else {
-        lastFillMonth = 12;
-      }
-    }
-
     const newFinData = {
       project_id: id,
       fin_types_id,
@@ -63,30 +35,7 @@ finDataRouter.post('/project/:id/findata/new', async (req, res) => {
       regular,
       start_date,
       end_date,
-      1: 0,
-      2: 0,
-      3: 0,
-      4: 0,
-      5: 0,
-      6: 0,
-      7: 0,
-      8: 0,
-      9: 0,
-      10: 0,
-      11: 0,
-      12: 0,
     };
-
-    for (let i = 1; i <= 12; i++) {
-      if (i === firstFillMonth) {
-        newFinData[i] = sum;
-      // eslint-disable-next-line max-len
-      } else if (i > firstFillMonth && i <= lastFillMonth && userStartYear - curYear <= 1 && regular === 'true') {
-        newFinData[i] = sum;
-      } else if (i > firstFillMonth && !lastFillMonth && regular === 'true') {
-        newFinData[i] = sum;
-      }
-    }
 
     await Fin_data.create(newFinData);
     res.status(201).json({ created: true });
@@ -102,64 +51,13 @@ finDataRouter.put('/findata/:id', async (req, res) => {
   } = req.body;
 
   try {
-    const userStartYear = Number(start_date.slice(0, 4));
-    const userStartMonth = Number(start_date.slice(5, 7));
-
-    const userEndYear = Number(end_date.slice(0, 4));
-    const userEndMonth = Number(end_date.slice(5, 7));
-
-    const curYear = new Date().getFullYear();
-    const curMonth = new Date().getMonth() + 1;
-
-    let firstFillMonth = null;
-    let lastFillMonth = null;
-
-    if (curYear === userStartYear && userStartMonth >= curMonth) {
-      firstFillMonth = userStartMonth - curMonth + 1;
-    } else if (userStartYear - curYear === 1 && userStartMonth < curMonth) {
-      firstFillMonth = 12 - curMonth + 1 + userStartMonth;
-    }
-
-    if (userEndYear) {
-      if (curYear === userEndYear && userEndMonth >= curMonth) {
-        lastFillMonth = userEndMonth - curMonth + 1;
-      } else if (userEndYear - curYear === 1 && userEndMonth < curMonth) {
-        lastFillMonth = 12 - curMonth + 1 + userEndMonth;
-      } else {
-        lastFillMonth = 12;
-      }
-    }
-
     const newFinData = {
       title,
       sum,
       regular,
       start_date,
       end_date,
-      1: 0,
-      2: 0,
-      3: 0,
-      4: 0,
-      5: 0,
-      6: 0,
-      7: 0,
-      8: 0,
-      9: 0,
-      10: 0,
-      11: 0,
-      12: 0,
     };
-
-    for (let i = 1; i <= 12; i++) {
-      if (i === firstFillMonth) {
-        newFinData[i] = sum;
-      // eslint-disable-next-line max-len
-      } else if (i > firstFillMonth && !lastFillMonth && regular === 'true') {
-        newFinData[i] = sum;
-      } else if (i > firstFillMonth && i <= lastFillMonth && userStartYear - curYear <= 1 && regular === 'true') {
-        newFinData[i] = sum;
-      }
-    }
 
     await Fin_data.update({ ...newFinData }, {
       where: { id },
