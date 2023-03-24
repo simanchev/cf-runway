4c2e033c3a6dbb4ffa9c8acb1a4da8a80de0f12e
anton <anttoon@me.com>
2023-03-24 16:40:49 +0300

    update config

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const { sequelize } = require('./db/models');
 const config = require('./config/config');
 
 const app = express();
-const PORT = process.env.PORT ?? 4000;
+const PORT = 5000;
 config(app);
 
 // app.listen(PORT, () => console.log(`Server started at ${PORT} port`));
